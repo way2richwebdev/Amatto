fdc0917e575d8498644e3776e027c1fdc652f553
markwebdev <markwebdev@mail.ru>
2026-07-08 19:58:56 +0300

    last v

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.menu__button');
+    const menu = document.querySelector('.header-content-bottom');
 
+    menuButton.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        menuButton.classList.toggle('active');
+    }); 
 });
